ec7300c02185b94e39d12e47e98b77ffcf550ac8
takuto.moriyasu <takuto.moriyasu@konicaminolta.com>
2019-12-03 14:19:19 +0900

    Oxy追加

--- WorkLoadManagement/AnalizeViewModel.cs
+++ WorkLoadManagement/AnalizeViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Wpf;
+using OxyPlot.Series;
 
 namespace WorkLoadManagement
 {
@@ -17,6 +20,7 @@ namespace WorkLoadManagement
             mycontrol = control;
             workdatalist = new ObservableCollection<WorkItem>();
             LoadWorkDataList();
+            LoadGraphData();
         }
 
         private void LoadWorkDataList()
@@ -25,6 +29,31 @@ namespace WorkLoadManagement
             {
                 workdatalist.Add(item);
             }
+        }
+        public void LoadGraphData()
+        {
+            Datalist = new List<DataPoint>();
+            _PlotModel = new PlotModel() { Title = "PieChart" };
+
+            Datalist.Add(new DataPoint(1, 2));
+            Datalist.Add(new DataPoint(1, 2));
+            Datalist.Add(new DataPoint(2, 3));
+
+            var series = new OxyPlot.Series.PieSeries
+            {
+                StrokeThickness = 2.0,
+                InsideLabelPosition = 0.5,
+                AngleSpan = 360,
+                StartAngle = 270,
+            };
+
+            series.Slices.Add(new PieSlice("Hathor", 7508));
+            series.Slices.Add(new PieSlice("CM-CT1", 6125));
+            series.Slices.Add(new PieSlice("Other", 4346));
+            series.Slices.Add(new PieSlice("None", 1778));
+
+            _PlotModel.Series.Add(series);
+
         }
 
         public ObservableCollection<WorkItem> WorkDataList
@@ -35,5 +64,8 @@ namespace WorkLoadManagement
             }
             
         }
+
+        public List<DataPoint> Datalist { get; set; }
+        public PlotModel _PlotModel { get; private set; } 
     }
 }

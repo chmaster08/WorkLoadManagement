de7ceb1ff5f769e00224972077199ccffcd297aa
chmaster <smart.tact.cheeser.0803@gmail.com>
2020-01-31 09:34:00 +0900

    CRUD変更

---
+++ WorkTimeManagementCore/CRUD.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkTimeManagementCore
+{
+
+
+    public class AllCRUD : ICRUDItem
+    {
+        public AllCRUD()
+        {
+
+        }
+
+        public void CreateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkItem GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WorkItem> ReadItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class DaylyCRUD : ICRUDItem
+    {
+        public DaylyCRUD()
+        {
+
+        }
+        public void CreateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkItem GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WorkItem> ReadItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MonthlyCRUD : ICRUDItem
+    {
+
+        public MonthlyCRUD()
+        {
+
+        }
+        public void CreateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WorkItem GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WorkItem> ReadItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
